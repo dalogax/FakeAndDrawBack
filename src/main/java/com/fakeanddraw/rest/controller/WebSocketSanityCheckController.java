e7767288b933e7fdd1968fd3664e250c6ebd7c9d
pedroml <pedromenchen@gmail.com>
2018-05-26 12:11:37 +0200

    No funciona - Pero para que Juan pueda trabajar

---
+++ src/main/java/com/fakeanddraw/rest/controller/WebSocketSanityCheckController.java
@@ -0,0 +1,20 @@
+package com.fakeanddraw.rest.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.fakeanddraw.rest.model.Greeting;
+import com.fakeanddraw.rest.model.HelloMessage;
+
+@Controller
+public class WebSocketSanityCheckController {
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/greetings")
+	public Greeting greeting(HelloMessage message) throws Exception {
+		Thread.sleep(1000); // simulated delay
+		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}	
+}
---
+++ src/main/java/com/fakeanddraw/rest/model/HelloMessage.java
@@ -0,0 +1,21 @@
+package com.fakeanddraw.rest.model;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

fa3db261ede55dea65e406f909cf7097a7af0ece
pedroml <pedromenchen@gmail.com>
2018-07-29 18:01:00 +0200

    Configuration test so Front-end can send big images

--- src/main/java/com/fakeanddraw/entrypoints/websocket/WebSocketConfig.java
+++ src/main/java/com/fakeanddraw/entrypoints/websocket/WebSocketConfig.java
@@ -4,10 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  @Override
+  public void configureWebSocketTransport(WebSocketTransportRegistration registry) {
+    registry.setMessageSizeLimit(200000); // default : 64 * 1024
+    registry.setSendTimeLimit(20 * 10000); // default : 10 * 10000
+    registry.setSendBufferSizeLimit(3 * 512 * 1024); // default : 512 * 1024
+  }
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
